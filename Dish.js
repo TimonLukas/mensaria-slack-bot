c3ee6f5dc5d680ce26dcc25ef61cfcc40acc7290
Timon Lukas <timon.lukas3@gmail.com>
2017-06-20 11:19:46 +0200

    Implement extraction of common elements

--- Dish.js
+++ Dish.js
@@ -1,11 +1,23 @@
 module.exports = class Dish {
   constructor (title, content, price) {
     this.title = title;
-    this.content = content;
+    this.contents = typeof content === "object" ? content : content.split(', ');
     this.price = price;
   }
 
   render () {
-    return `*${this.title}* _(${this.price})_\r\n${this.content}`;
+    return `*${this.title}* _(${this.price})_\r\n• ${this.contents.join('\r\n• ')}`;
+  }
+
+  filteredContents(commonElements) {
+    return new Dish(
+      this.title,
+      this.contents.filter(element => {
+        if(commonElements.indexOf(element) === -1) {
+          return element;
+        }
+      }),
+      this.price
+    );
   }
 };
--- app.js
+++ app.js
@@ -14,23 +14,36 @@ const app = express();
 app.get('/', (request, response) => {
   response.end('');
   util.hasRunToday().then(hasRun => {
-    if (!hasRun) {
+    if (!hasRun || process.env.DEBUG === 'true') {
       util.getHtml().then(html => {
         const dishes = util.getDishes(html);
         if (dishes.length > 0) {
           util.writeDate();
+
+          const contents = dishes.map(dish => dish.contents);
+          const commonElements = contents.shift().reduce((res, v) => {
+            if (res.indexOf(v) === -1 && contents.every(a => a.indexOf(v) !== -1)) {
+              res.push(v);
+            }
+            return res;
+          }, []);
+
           const dishMessage = dishes.map(dish => {
-            return dish.render();
+            return dish.filteredContents(commonElements).render();
           }).join('\r\n\r\n');
 
           const todaysDate = moment().format('DD.MM.YYYY');
-          const message = `<!channel> Das Mensa-Menü für den ${todaysDate}:\r\n\r\n${dishMessage}`;
+          const message = `<!channel> Das Mensa-Menü für den ${todaysDate}:\r\n• ${commonElements.join('\r\n• ')}\r\n\r\n${dishMessage}`;
 
-          client.chat.postMessage('mensa', message, (error, result) => {
-            if (typeof error !== 'undefined') {
-              throw error;
-            }
-          });
+          if (process.env.DEBUG !== 'true') {
+            client.chat.postMessage('mensa', message, (error, result) => {
+              if (typeof error !== 'undefined') {
+                throw error;
+              }
+            });
+          } else {
+            console.log(message);
+          }
         }
       });
     }
